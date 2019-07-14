759c0b056df2dbf4286cd47047771c17b171edc7
rafalcl <rafalclab@gmail.com>
2019-07-14 15:56:44 +0200

    Add sql to create users table. Add User model.

---
+++ sql/db.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE warsztat2krkw03
+--   CHARACTER SET utf8mb4
+--   COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE users (
+  id int AUTO_INCREMENT,
+  username varchar(255) UNIQUE NOT NULL,
+  email varchar(255) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY(id)
+);
