ae23cb87f2350a3f59af4f0e26705d614ccd7b25
rafalcl <rafalclab@gmail.com>
2019-07-14 16:36:47 +0200

    Add user DAO. Add Db class. Add Test class

---
+++ src/pl/coderslab/warsztat2krkw03/db/Db.java
@@ -0,0 +1,8 @@
+package pl.coderslab.warsztat2krkw03.db;
+
+public class Db {
+    public static final String URL =
+            "jdbc:mysql://localhost:3306/warsztat2krkw03?useSSL=false&characterEncoding=utf8";
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
+}
